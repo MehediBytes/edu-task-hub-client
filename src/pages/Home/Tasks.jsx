ed02a4e31509dbb650dcb8820331c640951775e7
mehedi20695 <mehedi.m000@gmail.com>
2025-02-21 01:33:56 +0600

    Implementing drag and drop task and update the task new category

--- src/pages/Home/Tasks.jsx
+++ src/pages/Home/Tasks.jsx
@@ -3,7 +3,8 @@ import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
 import useAxiosPublic from "../../hooks/UseAxiosPublic";
 import Swal from "sweetalert2";
 import { io } from "socket.io-client";
-import { FaTrash } from "react-icons/fa";
+import { FaRegTrashCan } from "react-icons/fa6";
+import { IoAddCircleOutline } from "react-icons/io5";
 
 const socket = io(import.meta.env.VITE_API_URL);
 
@@ -32,7 +33,7 @@ const Tasks = () => {
 
     // Add task
     const addTask = async () => {
-        if (!newTask.title) return Swal.fire("Please Put Title. Title is required!");
+        if (!newTask.title) return Swal.fire("Error", "Please Put Title. Title is required!", "error");
         if (newTask.title.length > 50) {
             return Swal.fire("Error", "Title must be 50 characters or less!", "error");
         }
@@ -67,7 +68,7 @@ const Tasks = () => {
                     }
                 } catch (error) {
                     console.error("Error deleting request:", error);
-                    Swal.fire("Error", "Failed to delete the request.", "error", error.message);
+                    Swal.fire("Error", "Failed to delete the task.", "error", error.message);
                 }
             }
         });
@@ -79,15 +80,17 @@ const Tasks = () => {
         const updatedTasks = [...tasks];
         const [movedTask] = updatedTasks.splice(result.source.index, 1);
         movedTask.category = result.destination.droppableId;
+
         updatedTasks.splice(result.destination.index, 0, movedTask);
         setTasks(updatedTasks);
+        
         await axiosPublic.put(`/tasks/${movedTask._id}`, movedTask);
     };
 
     return (
         <div className="container mx-auto px-3">
             <h1 className="text-3xl font-bold text-center mb-5 text-purple-500">Task Management System</h1>
-            <div className="bg-base-100 p-5 rounded-lg">
+            <div>
                 <input
                     type="text"
                     name="title"
@@ -107,7 +110,7 @@ const Tasks = () => {
                 {/* Category Dropdown */}
                 <select
                     name="category"
-                    className="select select-bordered text-gray-400 w-full mb-3"
+                    className="select select-bordered w-full mb-3"
                     value={newTask.category}
                     onChange={handleChange}
                 >
@@ -116,19 +119,29 @@ const Tasks = () => {
                     <option value="Done">Done</option>
                 </select>
 
-                <button className="btn bg-purple-500 text-base-100 hover:bg-purple-700 w-full" onClick={addTask}>
-                    Add Task
+                <button
+                    className="btn bg-purple-500 text-base-100 hover:bg-purple-700 w-full text-lg flex justify-center items-center"
+                    onClick={addTask}>
+                    <span>Add Task</span> <IoAddCircleOutline />
                 </button>
+            </div>
+
+            <div className="mt-5">
+                <h3 className="text-xl font-bold">All Tasks On The Basis Of Categories:</h3>
             </div>
 
             <DragDropContext onDragEnd={handleDragEnd}>
                 <div className="grid grid-cols-3 gap-4 mt-5">
+
                     {["To-Do", "In Progress", "Done"].map(category => (
                         <Droppable key={category} droppableId={category}>
                             {(provided) => (
-                                <div {...provided.droppableProps} ref={provided.innerRef}
-                                    className="bg-purple-200 p-4 rounded-lg min-h-screen">
-                                    <h2 className="text-xl font-semibold mb-3 text-center text-purple-500">{category}</h2>
+                                <div {...provided.droppableProps}
+                                    ref={provided.innerRef}
+                                    className="bg-purple-200 p-4 rounded-lg min-h-[250px]">
+
+                                    <h2 className="text-xl font-bold mb-3 text-center text-purple-500">{category}</h2>
+
                                     {tasks.filter(task => task.category === category)
                                         .map((task, index) => (
                                             <Draggable key={task._id} draggableId={task._id} index={index}>
@@ -137,8 +150,8 @@ const Tasks = () => {
                                                         className="bg-base-100 p-3 rounded-lg mb-2 shadow-md flex flex-col justify-center items-center">
                                                         <h3 className="font-bold">{task.title}</h3>
                                                         <p>{task.description}</p>
-                                                        <button className="btn btn-sm btn-error mt-2"
-                                                            onClick={() => deleteTask(task._id)}><FaTrash></FaTrash></button>
+                                                        <button className="btn btn-sm btn-error mt-2 rounded-full"
+                                                            onClick={() => deleteTask(task._id)}><FaRegTrashCan /></button>
                                                     </div>
                                                 )}
                                             </Draggable>
